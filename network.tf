ed422ff79b74ac0eef30de137530f4da8a8e4490
SergioHerruzo <2223_sergio.herruzo@iticbcn.cat>
2026-04-28 16:45:03 +0200

    a

--- network.tf
+++ network.tf
@@ -15,7 +15,7 @@ resource "aws_acm_certificate" "cert" {
 
 resource "cloudflare_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.resource_record_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name    = dvo.resource_record_name
       content = dvo.resource_record_value
       type    = dvo.resource_record_type
